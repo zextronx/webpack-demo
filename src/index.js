e5e6f10b30f22eebe8c72a01cd4f65fed059377e
Aditya Gaur <adityagaur1994@gmail.com>
2022-08-09 13:05:50 +0530

    added html webpack plugin for document accessibility

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import { addNum } from './utils/math';
 
 import './index.css';
 
-const result1 = addNum(1, 3);
+console.log('Webpack tutorial')
 
+const result1 = addNum(1, 3);
 console.log(result1);
 
 const result2 = result1 + addNum(5, 3);
-
 console.log(result2);
+
+const headingDiv = document.createElement('div');
+
+const newContent = document.createTextNode("Hi there and greetings!");
+headingDiv.appendChild(newContent);
+
+document.body.appendChild(headingDiv);
